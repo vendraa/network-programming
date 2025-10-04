f72860d0904fe5381f7df517ffe4b3a13ff1dfd6
Novendra Anugrah <novendraanugrah@gmail.com>
2025-10-04 23:10:43 +0700

    Added chatserver

---
+++ chatserver.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define MAX_CLIENTS 10
+#define BUFFER_SIZE 1024
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <Port>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    int server_fd, new_socket, client_socket[MAX_CLIENTS];
+    struct sockaddr_in address;
+    int max_sd, activity, valread, sd;
+    socklen_t addrlen;
+    char buffer[BUFFER_SIZE];
+
+    fd_set readfds;
+    int port = atoi(argv[1]);
+
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        client_socket[i] = 0;
+    }
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("Socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Chat server running on port %d (PID=%d)\n", port, getpid());
+
+    addrlen = sizeof(address);
+
+    while (1) {
+        FD_ZERO(&readfds);
+        FD_SET(server_fd, &readfds);
+        max_sd = server_fd;
+
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            sd = client_socket[i];
+            if (sd > 0) FD_SET(sd, &readfds);
+            if (sd > max_sd) max_sd = sd;
+        }
+
+        activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
+
+        if ((activity < 0)) {
+            perror("Select error");
+        }
+
+        if (FD_ISSET(server_fd, &readfds)) {
+            new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen);
+            if (new_socket < 0) {
+                perror("Accept error");
+                exit(EXIT_FAILURE);
+            }
+
+            printf("New connection: socket fd=%d, ip=%s, port=%d\n",
+                   new_socket,
+                   inet_ntoa(address.sin_addr),
+                   ntohs(address.sin_port));
+
+            for (int i = 0; i < MAX_CLIENTS; i++) {
+                if (client_socket[i] == 0) {
+                    client_socket[i] = new_socket;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            sd = client_socket[i];
+
+            if (FD_ISSET(sd, &readfds)) {
+                if ((valread = read(sd, buffer, BUFFER_SIZE)) == 0) {
+                    getpeername(sd, (struct sockaddr *)&address, &addrlen);
+                    printf("Client disconnected: ip=%s, port=%d\n",
+                           inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+                    close(sd);
+                    client_socket[i] = 0;
+                } else {
+                    buffer[valread] = '\0';
+                    printf("Received from fd=%d: %s", sd, buffer);
+
+                    for (int j = 0; j < MAX_CLIENTS; j++) {
+                        if (client_socket[j] != 0 && client_socket[j] != sd) {
+                            send(client_socket[j], buffer, strlen(buffer), 0);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return 0;
+}
